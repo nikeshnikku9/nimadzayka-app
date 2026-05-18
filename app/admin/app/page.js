f3016ac5f1a827314fa3fcbc5be2a1cbd0191034
Nikeshnikku <55690082+nikeshnikku9@users.noreply.github.com>
2026-05-19 02:28:33 +0530

    Delete app/admin/app directory

--- app/admin/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:"40px",fontFamily:"Arial"}}>
-      <h1>Nimad ZAYKA</h1>
-      <p>Premium Spice Brand Platform</p>
-    </main>
-  );
-}
