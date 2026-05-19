c0cf136f80e01a86bf80a6271cb09d5a8bb5aed8
Nikeshnikku <55690082+nikeshnikku9@users.noreply.github.com>
2026-05-19 15:45:26 +0530

    Update page.js

--- app/admin/barcode/page.js
+++ app/admin/barcode/page.js
@@ -1,15 +1,20 @@
 'use client';
 
-import { useState, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import JsBarcode from 'jsbarcode';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 export default function BarcodePage() {
 
-  const [value, setValue] = useState('890123456789');
   const svgRef = useRef(null);
+  const printRef = useRef(null);
 
-  const generateBarcode = () => {
+  const [code, setCode] = useState('8901234567890');
+  const [productName, setProductName] = useState('NIMAD ZAYKA HALDI');
+  const [price, setPrice] = useState('₹70');
+  const [weight, setWeight] = useState('100g');
 
-    JsBarcode(svgRef.current, value, {
-      format: 'CODE128',
-     
+  useEffect(() => {
+
+    if (!svgRef.current)
