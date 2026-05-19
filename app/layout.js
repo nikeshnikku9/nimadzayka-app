95f6b8d038e86e33969d94c1278af6ca8e6e33e4
Nikeshnikku <55690082+nikeshnikku9@users.noreply.github.com>
2026-05-19 14:07:33 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Nimad ZAYKA',
-  description: 'Premium Indian Spices',
-}
+  title: "Nimad ZAYKA",
+  description: "Premium Indian Spices",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
